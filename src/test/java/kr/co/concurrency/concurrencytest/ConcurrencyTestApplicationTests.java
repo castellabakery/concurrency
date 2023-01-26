44216f1aab75a7c1c5adc67f52d3ef428c4181b4
mhpark <mhpark@kanak.co.kr>
2023-01-26 17:03:57 +0900

    테스트 코드 작성

--- src/test/java/kr/co/concurrency/concurrencytest/ConcurrencyTestApplicationTests.java
+++ src/test/java/kr/co/concurrency/concurrencytest/ConcurrencyTestApplicationTests.java
@@ -33,7 +33,7 @@ class ConcurrencyTestApplicationTests {
     @DisplayName("Increase Test")
     void 증가테스트() throws InterruptedException {
         int threadCount = 10000;
-        //멀티스레드 이용 ExecutorService : 비동기를 단순하게 처리할 수 있또록 해주는 java api
+        //멀티스레드 이용 ExecutorService : 비동기를 단순하게 처리할 수 있도록 해주는 java api
         ExecutorService executorService = Executors.newFixedThreadPool(32);
 
         //다른 스레드에서 수행이 완료될 때 까지 대기할 수 있도록 도와주는 API - 요청이 끝날때 까지 기다림
@@ -64,7 +64,7 @@ class ConcurrencyTestApplicationTests {
     @DisplayName("Decrease Test")
     void 감소테스트() throws InterruptedException {
         int threadCount = 19000;
-        //멀티스레드 이용 ExecutorService : 비동기를 단순하게 처리할 수 있또록 해주는 java api
+        //멀티스레드 이용 ExecutorService : 비동기를 단순하게 처리할 수 있도록 해주는 java api
         ExecutorService executorService = Executors.newFixedThreadPool(32);
 
         //다른 스레드에서 수행이 완료될 때 까지 대기할 수 있도록 도와주는 API - 요청이 끝날때 까지 기다림
